c35816fe75f279f52f08901369b7ec7515834197
SxxCodezx <92841319+SxxCodzx@users.noreply.github.com>
2022-08-09 20:09:45 -0500

    ItemIds-PM4

---
+++ src/SxxCodezx/ItemIds.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SxxCodezx;
+
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class ItemIds extends PluginBase implements Listener {
+
+    public function onEnable(): void{
+        $this->getLogger()->info("Item Ids Enable");
+        Server::getInstance()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function getItemId(PlayerItemHeldEvent $ev){
+        $player = $ev->getPlayer();
+        if($player->hasPermission(DefaultPermissions::ROOT_OPERATOR)){
+            $item = $ev->getItem()->getId();
+            if($item === 0){
+                
+            }else{
+                $meta = $ev->getItem()->getMeta();
+                $player->sendTip("§8ItemId§f §5".$item."§f:§5".$meta);
+            }
+        }
+    }
+}
